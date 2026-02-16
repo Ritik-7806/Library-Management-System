fe3f53da051700d6a822d79b43c774bbfef10032
Ritik Sharma <ritik.rs7806@gmail.com>
2026-02-16 21:33:35 +0530

    final touch

--- src/main/java/com/example/lms/Controller/HealthCheckController.java
+++ src/main/java/com/example/lms/Controller/HealthCheckController.java
@@ -10,8 +10,30 @@ public class HealthCheckController {
 
     @GetMapping("/")
     public ResponseEntity<String> healthCheck(){
-        return ResponseEntity.status(HttpStatus.OK).body("All the APIs and available endpoints can be accessed and tested via Swagger UI at the following URL:\n" +
-                "\uD83D\uDC49 http://localhost:8081/swagger-ui/index.html") ;
+        String msg = """
+        <h2>Library Management System</h2>
+
+        <p>All the APIs and available endpoints can be accessed and tested via <b>Swagger UI</b>.</p>
+
+        <p>
+        <a href="http://localhost:8081/swagger-ui/index.html">
+        Open Swagger UI
+        </a>
+        </p>
+
+        <h3>Features:</h3>
+        <ul>
+            <li>JWT-based Authentication & Authorization</li>
+            <li>Role-based access (ADMIN / USER)</li>
+            <li>Secure Login & Signup</li>
+            <li>Book Management APIs</li>
+            <li>Stateless Session using Spring Security</li>
+        </ul>
+        """;
+
+        return ResponseEntity.ok()
+                .header("Content-Type", "text/html")
+                .body(msg);
     }
 
 }
